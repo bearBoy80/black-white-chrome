79b7d4211471e5e007dd20d61953670dcc4657d8
bearboy80 <2373319771@qq.com>
2024-09-25 12:02:54 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Black and White #
+
+When activated, this Chrome extension disables borders, outlines, background colors, text colors, box shadows and text shadows.
+
+Seeing a web page in such a rudimentary form helps you evaluate the information hierarchy of your design. Suddenly it becomes much easier to make sure you are adhering to a coherent visual grid. Without the visual clutter it allows you to see the typographic structure a lot easier.
+
+## Examples ##
+
+### nytimes.com ###
+
+![Screenshot of The New York Times](https://cloud.githubusercontent.com/assets/704336/3599572/3ad2412e-0cf0-11e4-9578-8f738f3cbe87.png)
+
+### github.com ###
+
+![Screenshot of Github](https://cloud.githubusercontent.com/assets/704336/3599573/3af82ea2-0cf0-11e4-9f31-be226e12656d.png)
+
+### en.wikipedia.org ###
+
+![Screenshot of Wikipedia](https://cloud.githubusercontent.com/assets/704336/3599574/3b065d24-0cf0-11e4-94c9-bb551e9f4a75.png)
+
+### google.com ###
+
+![Screenshot of Google search results](https://cloud.githubusercontent.com/assets/704336/3599575/3b0eb258-0cf0-11e4-82d0-440812f05093.png)
+
+## Installation ##
+
+Get the extension from the [Releases section](https://github.com/JannesMeyer/chrome-black-and-white/releases) or from the [Chrome Web Store](https://chrome.google.com/webstore/detail/black-and-white/amdnjmpcoialaikdldjbnkipmckdheie).
+
+## License ##
+
+[GPLv3](http://www.gnu.org/licenses/gpl-3.0.txt)
---
+++ src/event-page.js
@@ -0,0 +1,137 @@
+var states = {
+	active: {
+		icons: {
+			'19': 'icon-color-19.png',
+			'38': 'icon-color-38.png'
+		},
+		title: 'Turn black and white mode off'
+	},
+	inactive: {
+		icons: {
+			'19': 'icon-bw-19.png',
+			'38': 'icon-bw-38.png'
+		},
+		title: 'Turn black and white mode on'
+	}
+};
+function activateBlackAndWhiteMode() {
+	document.documentElement.classList.add("black-and-white-mode");
+	console.log("Black and white mode activated");
+}
+
+function deactivateBlackAndWhiteMode() {
+	document.documentElement.classList.remove("black-and-white-mode");
+	console.log("Black and white mode deactivated");
+}
+
+function setActive(activate, tabId) {
+	console.log('setActive called with activate:', activate, 'tabId:', tabId);
+	var props = activate ? states.active : states.inactive;
+	chrome.action.setIcon({ path: props.icons, tabId: tabId });
+	chrome.action.setTitle({ title: props.title, tabId: tabId });
+	chrome.scripting.executeScript({ 
+		target: { tabId: tabId, allFrames: true }, 
+		func: activate ? activateBlackAndWhiteMode : deactivateBlackAndWhiteMode
+	}, (results) => {
+		if (chrome.runtime.lastError) {
+			console.error('Error executing script:', chrome.runtime.lastError);
+		} else {
+			console.log('Script executed successfully:', results);
+			// Save the state for this tab's URL
+			chrome.tabs.get(tabId, (tab) => {
+				const url = new URL(tab.url).hostname;
+				chrome.storage.local.set({[url]: activate}, () => {
+					if (chrome.runtime.lastError) {
+						console.error('Error saving state:', chrome.runtime.lastError);
+					} else {
+						debugStorage('set', url, activate);
+					}
+				});
+			});
+		}
+	});
+}
+
+chrome.action.onClicked.addListener(function(tab) {
+	console.log('Action clicked for tab:', tab.id);
+	chrome.action.getTitle({ tabId: tab.id }, function(title) {
+		console.log('Current title:', title);
+		if (title !== states.active.title) {
+			console.log('Activating black and white mode');
+			chrome.scripting.insertCSS({ 
+				target: { tabId: tab.id, allFrames: true }, 
+				files: ['black-and-white.css']
+			}, function() {
+				if (chrome.runtime.lastError) {
+					console.error('Error inserting CSS:', chrome.runtime.lastError);
+					return;
+				}
+				console.log('CSS inserted successfully');
+				setActive(true, tab.id);
+			});
+		} else {
+			console.log('Deactivating black and white mode');
+			chrome.scripting.removeCSS({ 
+				target: { tabId: tab.id, allFrames: true }, 
+				files: ['black-and-white.css']
+			}, function() {
+				if (chrome.runtime.lastError) {
+					console.error('Error removing CSS:', chrome.runtime.lastError);
+					return;
+				}
+				console.log('CSS removed successfully');
+				setActive(false, tab.id);
+			});
+		}
+	});
+});
+
+// Add this new listener for tab updates
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.status === 'complete' && tab.url) {
+		const url = new URL(tab.url).hostname;
+		console.log("get url:" + url);
+		chrome.storage.local.get(url, (result) => {
+			debugStorage('get', url, result[url]);
+			if (chrome.runtime.lastError) {
+				console.error('Error retrieving state:', chrome.runtime.lastError);
+				return;
+			}
+			if (result[url]) {
+				chrome.scripting.insertCSS({ 
+					target: { tabId: tabId, allFrames: true }, 
+					files: ['black-and-white.css']
+				}, function() {
+					if (chrome.runtime.lastError) {
+						console.error('Error inserting CSS:', chrome.runtime.lastError);
+						return;
+					}
+					console.log('CSS inserted successfully');
+					setActive(true, tabId);
+				});
+			}
+		});
+	}
+});
+
+// 添加一个新的监听器来测试存储
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.storage.local.set({test: 'Hello, Storage!'}, () => {
+		if (chrome.runtime.lastError) {
+			console.error('Error setting test value:', chrome.runtime.lastError);
+		} else {
+			debugStorage('set', 'test', 'Hello, Storage!');
+			chrome.storage.local.get('test', (result) => {
+				debugStorage('get', 'test', result.test);
+			});
+		}
+	});
+});
+
+// 在文件顶部添加这个函数
+function debugStorage(action, key, value) {
+	console.log(`Storage ${action}:`, { key, value });
+	chrome.storage.local.get(null, function(items) {
+		console.log('All storage items:', items);
+	});
+}
